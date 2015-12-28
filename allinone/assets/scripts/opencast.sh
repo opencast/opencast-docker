34eca94df63da39436341a515a75d727b3b1b1ca
Matthias Neugebauer <matthias.neugebauer@uni-muenster.de>
2015-12-28 12:09:20 +0100

    Fix error where settings were set in the wrong file

--- allinone/assets/scripts/opencast.sh
+++ allinone/assets/scripts/opencast.sh
@@ -36,7 +36,7 @@ Opencast::Opencast::Configure() {
     "ORG_OPENCASTPROJECT_FILE_REPO_URL"
 
   if ! Opencast::Helper::Dist::allinone; then
-    Opencast::Helper::ReplaceInfile "etc/custom.properties" \
+    Opencast::Helper::ReplaceInfile "etc/org.opencastproject.organization-mh_default_org.cfg" \
       "PROP_ORG_OPENCASTPROJECT_ADMIN_UI_URL" \
       "PROP_ORG_OPENCASTPROJECT_ENGAGE_UI_URL"
   fi
